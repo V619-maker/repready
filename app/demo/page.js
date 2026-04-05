9bfc72488d347c89c13617f2e47bcb0e086bc590
V619-maker <vrushalkitke123@gmail.com>
2026-04-05 19:47:55 +0530

    Create page.js

---
+++ app/demo/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import RepReadyCoach from '@/components/RepReadyCoach';
+
+export default function DemoPage() {
+  return (
+    <main className="min-h-screen bg-black">
+      <RepReadyCoach />
+    </main>
+  );
+}
