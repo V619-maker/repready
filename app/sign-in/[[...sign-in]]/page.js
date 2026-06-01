1489c3d640666acd3b7addff9abf20913213cb93
V619-maker <vrushalkitke123@gmail.com>
2026-06-01 23:44:11 +0530

    Update page.js

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex items-center justify-center w-full min-h-[calc(100vh-80px)] py-12">
-      <SignIn forceRedirectUrl="/simulate" />
+     <SignIn forceRedirectUrl="/deck" />
     </div>
   );
 }
